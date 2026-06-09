08cd5ae5915ca8f083986b660ea0e78cfa870284
Tanishq Parashar <tanishqparashar123@gmail.com>
2026-06-09 19:13:30 +0530

    [Tanishq Parashar].Add solved first problem of level1

---
+++ core-java-practice/programming-elements/level1/HarshAge.java
@@ -0,0 +1,11 @@
+package programmingelements.level1;
+
+public class HarshAge {
+    public static void main(String[] args) {
+
+        int birthYear=2000;
+        int curreent=2024;
+        int age=curreent-birthYear;
+        System.out.print("Harsh Age is "+age);
+    }
+}
